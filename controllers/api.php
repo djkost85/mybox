af737b31d6b907bd00bf3115ed256f0182d2ad41
ildar <wajox@mail.ru>
2012-09-16 15:08:39 +0400

    api getPosts

--- controllers/api.php
+++ controllers/api.php
@@ -2,7 +2,27 @@
 namespace box;
 
 class api {
-  public function setToken(Silex\Application $app) {
-    var_dump($app);
+
+  public function setToken( Silex\Application $app ) {
+
+    var_dump( $app );
+
   }
+
+  public function getPosts( Silex\Application $app ) {
+
+     $provider = ''; // get provider name
+     
+     $token = ''; // load token
+     
+     require_once __DIR__.'/../models/'.$provider.'Provider.php'; // include provider class
+     
+     $class = 'provider\\'.$provider.'Provider';
+
+     $provider = new $class( $token );
+     
+     echo call_user_func_array( array( $provider, "getPosts" ), array() );
+  
+  }
+
 }
