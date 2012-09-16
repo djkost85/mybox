eb7d5c5b53652cc60b378d61a31998878ca28b11
NeonXP <frei@neonxp.info>
2012-09-16 16:50:43 +0400

    vk

--- web/index.php
+++ web/index.php
@@ -1,21 +1,42 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Bridge\Twig\TwigEngine;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../controllers/api.php';
 require_once __DIR__ . '/../vendor/php-activerecord/php-activerecord/ActiveRecord.php';
+require_once __DIR__ . '/../vendor/ukko/vk/src/VK.php';
+
+$app = new Silex\Application();
 
 \ActiveRecord\Config::initialize(function($cfg)
 {
      $cfg->set_model_directory(__DIR__.'/../models');
      $cfg->set_connections(array(
-         'development' => 'mysql://'.$_SERVER["DB1_USER"].':'.$_SERVER["DB1_PASS"].'@'.$_SERVER["DB1_HOST"].'/'.$_SERVER["DB1_NAME"]));
+       /*  'production' => 'mysql://'.$_SERVER["DB1_USER"].':'.$_SERVER["DB1_PASS"].'@'.$_SERVER["DB1_HOST"].'/'.$_SERVER["DB1_NAME"], */
+         'development' => 'mysql://root:18445610@localhost/db1'
+     ));
 });
 
-$app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
 
 $app['debug'] = true;
+$app['vk'] = new VK("3127460", "578a4YiVuVWpTUqaltxB");
+
+$app->get("/", function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->get("/login_vk", function () use ($app) {
+    $app['vk']->getAuthorizeURL('2', 'http://mybox.pagodabox.com/login_vk_callback');
+});
+
+$app->get("/login_vk_callback", function (Request $request) use ($app) {
+    var_dump($request);
+});
+
 
 $app->post('/api/set_token', function (Request $request) {
     $token = $request->get('token');
